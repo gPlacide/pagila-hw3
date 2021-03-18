1d2c9fcfaa56c539935da7a9bc9869dfd9e53b00
Placide Gatabazi <pgatabazi21@cmc.edu>
2021-03-18 01:27:39 -0700

    fixed sql5

--- sql/05.sql
+++ sql/05.sql
@@ -5,7 +5,7 @@
  * Create a select statement that lists the titles of all tables with the 'Behind the Scenes' special_feature.
  * Create a select statement that lists the titles of all tables with the 'Trailers' special_feature.
  * Inner join the queries above.
-*/ 
+ 
 CREATE view temporary_behind_the_scenes AS (
     SELECT
         title
@@ -34,5 +34,35 @@ SELECT
     title
 FROM temporary_behind_the_scenes
 INNER JOIN temporary_trailers USING (title)
+ORDER BY title
+;
+*/
+SELECT
+    title
+FROM (
+    SELECT
+        title
+    FROM (
+    SELECT
+        title, 
+        film_id,
+        unnest(special_features)
+    FROM film) AS p
+    WHERE unnest = 'Behind the Scenes'
+) AS pl
+INNER JOIN
+(SELECT
+    title
+FROM (
+    SELECT
+        title
+    FROM (
+        SELECT
+            title, 
+            film_id,
+            unnest(special_features)
+        FROM film) AS pla
+    WHERE unnest = 'Trailers') AS plac ) AS placi
+USING (title)
 ORDER BY title
 ;
